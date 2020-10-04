276c54d11c45394bd31d4b834b18e49cff498529
0x416c616e <29168822+0x416c616e@users.noreply.github.com>
2020-10-03 22:56:55 -0500

    fixed JAR and now program can be run with RUN_BENCHMARK.bat

--- run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd out
-cd artifacts
-cd java15_jar_test_jar
-java --module-path "..\..\..\openjfx-15_windows-x64_bin-sdk\javafx-sdk-15\lib" --add-modules javafx.controls,javafx.fxml,javafx.graphics,javafx.web -jar java15_jar_test.jar
--- run.vbs
+++
@@ -1,4 +0,0 @@
-Set oShell = CreateObject ("Wscript.shell")
-Dim strArgs
-strArgs = "cmd /c run.bat"
-oShell.Run strArgs, 0, false
